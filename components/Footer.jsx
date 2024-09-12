8ce4503c8e6ddb2f6c34ab52d38b5c051d407b55
martinstereo <martinsteiro02@gmail.com>
2024-09-12 13:43:04 +0200

    dark mode

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-white text-black">
-            <div className="container text-sm mx-auto py-6 text-center">
-            <hr className="h-px my-8 border-0 dark:bg-slate-200"/>
-                <p>Nettside utviklet av Jonas Fritzøe Hovdenak, Sander Javier Nomedal, Martin Steiro, Ruben Teikari & Johannes Tjøstheim - 2024</p>
+        
+        <footer className="bg-white dark:bg-slate-950 text-gray-800 dark:text-gray-300">
+            <div className="container text-xs mx-auto py-6 text-center">
+            <hr className="h-px my-8 border-0 bg-gray-200 dark:bg-gray-700" />
+                <p>Nettsiden er utviklet av Jonas Fritzøe Hovdenak, Sander Javier Nomedal, Martin Steiro, Ruben Teikari & Johannes Tjøstheim - 2024</p>
             </div>
         </footer>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "selector",
   theme: {
     extend: {
       fontFamily: {
         inter: ['Inter var', 'sans-serif'], // Add your custom font here
-        sans: [
-          'Inter var',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
       }
     },
   },
